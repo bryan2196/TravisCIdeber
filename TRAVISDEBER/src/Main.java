dd8c2e62c6c0d90127181454f15c9fa95a7972b8
Bryan Macias <Bryan Macias@DESKTOP-F5CMJ2L>
2016-09-05 21:23:42 -0500

    prueba

---
+++ TRAVISDEBER/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ 
+ * @author Bryan Macias
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int valor1= 0;
+        int valor2= 0;
+        int resultado;
+        System.out.println("Ingresa el primer valor");
+        valor1= reader.nextInt();
+        System.out.println("Ingrese el segundo valor");
+        valor2= reader.nextInt();
+        resultado =valor1+valor2;
+       
+        System.out.println("la suma es"+valor1+"+"+valor2+"="+resultado);
+
+    }
+    
+}
